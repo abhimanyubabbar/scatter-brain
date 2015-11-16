a082de1441ab0971655abffc7fc7ab83f222022d
Abhimanyu Babbar <abhimanyu.babbar@wrapp.com>
2015-11-16 23:46:22 +0100

    Fixed the file serving issue.

---
+++ server.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"os"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	http.HandleFunc("/api/ping", pingHandler)
+	http.Handle("/", http.FileServer(http.Dir("public")))
+	http.ListenAndServe(":"+port, nil)
+}
+
+type Status struct {
+	Status  string
+	Service string
+}
+
+func pingHandler(rw http.ResponseWriter, r *http.Request) {
+
+	response := Status{"pong", "scatter-brain"}
+	resp, _ := json.Marshal(response)
+	rw.Write(resp)
+}
