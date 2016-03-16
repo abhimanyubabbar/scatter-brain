e89c3d10c7dca8e27f2c0261b84421bc6da7d086
Abhimanyu Babbar <abhimanyu.babbar@wrapp.com>
2016-03-16 20:27:48 +0100

    Fixed landing page to show entries titles.

--- public/js/landing.jsx
+++ public/js/landing.jsx
@@ -2,11 +2,52 @@ import React from 'react';
 
 var Landing = React.createClass({
 
+  getInitialState : function(){
+    return {
+      thoughts: []
+    };
+  },
+
+  componentDidMount : function(){
+    this.loadAllThoughts();
+  },
+
+  loadAllThoughts : function(){
+
+    $.ajax({
+      url: '/api/thoughts',
+      dataType: 'json',
+      success: function(data) {
+        console.log("Received thought array");
+        var thoughts = [];
+        for (var i=0; i< data.length; i++) {
+          thoughts.push(data[i]);
+        }
+        this.setState({
+          thoughts:thoughts
+        });
+      }.bind(this),
+
+      error: function(err) {
+        console.log(err);
+      }.bind(this)
+    });
+  },
+
   render : function(){
+
+    var divArray = [];
+    var thoughts = this.state.thoughts;
+
+    for( var i=0; i < thoughts.length ; i++) {
+      divArray.push(
+        <h3>{thoughts[i].title}</h3>
+      );
+    }
 
     return(
       <div>
-        <p>Main Landing Page.</p>
+        {divArray}
       </div>
     );
   }
